40c21d5fed8f10f419029f7c9983c8aff917378f
MacWilliam <874681044@qq.com>
2020-02-19 13:36:54 +0800

    class+end

--- model.py
+++ model.py
@@ -58,7 +58,54 @@ def create_lstm_baseline_model(audio_tuple_input, W, B,
     return tf.matmul(outputs[-1], W) + B
     #shape:   (batchsize*tuplesize, dimension_linear_layer)
 
-def my_tuple_loss(batch_size, tuple_size, spk_representation, labels, l_weight, l_bias):
+def create_lstm_class_model(audio_tuple_input, lstm_model_setting, dropout_prob):
+    '''
+    audio_tuple_input: shape:    (bash_size, tuple_size=num_utt_enrollment+1, time_steps, num_features)
+    '''
+    #reshape inputdata
+    shape_input_data = audio_tuple_input.shape
+    batch_size = shape_input_data[0]
+    tuple_size = shape_input_data[1]
+    time_steps = shape_input_data[2]
+    feature_size = shape_input_data[3]
+
+    X = tf.reshape(audio_tuple_input, [batch_size*tuple_size, time_steps, feature_size])
+    XT = tf.transpose(X, [1, 0, 2])
+    #XT:  (time_steps, batchsize*tuplesize, featuresize)
+    XR = tf.reshape(XT, [-1, feature_size])
+    #XR: (batchsize*tuplesize*timesteps, featuressize)
+    X_split = tf.split(XR, time_steps, 0)
+    # X_split:   timesteps arrays, each array has the dimension (bashsize*tuplesize,featuresize)
+
+    #definit lstm
+    num_units = lstm_model_setting['num_units'] # 128
+    dimension_projection = lstm_model_setting['dimension_projection'] # 64
+    num_layers = lstm_model_setting['num_layers'] # 3
+    mult_lstm = tf.nn.rnn_cell.MultiRNNCell([unit_lstm(num_units, dimension_projection, dropout_prob) for i in range(num_layers)],
+        state_is_tuple=True)
+
+    #get output of lstm
+    outputs, _states = tf.contrib.rnn.static_rnn(mult_lstm, X_split, dtype=tf.float32)
+
+    # define projection layer, dimension of output:  timesteps arrays and each arrays has
+    # dimension: (batchsize*tuplesize, outputsize).  we just want to take the output of last layer
+    dimension_linear_layer = lstm_model_setting['dimension_linear_layer']
+    W1 = tf.Variable(tf.random_normal([dimension_projection, dimension_linear_layer], stddev=1), name='weights')
+    B1 = tf.Variable(tf.random_normal([dimension_linear_layer], stddev=1), name='bias')
+
+    speaker_vector = tf.matmul(outputs[-1], W1) + B1
+    # define classification layer
+    num_class = lstm_model_setting['num_class']
+    W2 = tf.Variable(tf.random_normal([dimension_linear_layer, num_class], stddev=1), name='weights')
+    B2 = tf.Variable(tf.random_normal([num_class], stddev=1), name='bias')
+
+    logits = tf.nn.softmax(tf.matmul(speaker_vector, W2) + B2)
+
+    return speaker_vector, logits
+    # speaker_vector shape:   (batchsize*tuplesize, dimension_linear_layer)
+    # logits shape:           (batchsize*tuplesize, num_class)
+
+def my_tuple_loss(batch_size, tuple_size, spk_representation, labels):
     '''
     this function can calcul the tuple loss for a batch
     spk_representation:    (bashsize*tuplesize, dimension of linear layer)
@@ -70,9 +117,7 @@ def my_tuple_loss(batch_size, tuple_size, spk_representation, labels, l_weight, 
     w = tf.reshape(spk_representation, [batch_size, tuple_size, feature_size])
 
     loss = 0
-    # cos_score = []
-    # p_cos_score = []
-    # cos_label = []
+
     for indice_bash in range(batch_size):
         # vec[1:] is enroll vectors
         wi_enroll = w[indice_bash, 1:]    # shape:  (tuple_size-1, feature_size)
@@ -90,26 +135,13 @@ def my_tuple_loss(batch_size, tuple_size, spk_representation, labels, l_weight, 
         cos_similarity = tf.reduce_sum(tf.multiply(normlize_ck, normlize_wi_eval))
 
         score = cos_similarity
-        p_score = tf.add(tf.multiply(-l_weight, score), -l_bias)
         label = tf.cast(labels[indice_bash], dtype=tf.float32)
 
-        # cos_score.append(score.eval())
-        # p_cos_score.append(p_score.eval())
-        # cos_label.append(label.eval())
-        #
-        # pdb.set_trace()
-
-        loss_one = tf.multiply(label, tf.log(tf.sigmoid(p_score)))
-        loss_zero = tf.multiply((1-label), tf.log((1 - tf.sigmoid(p_score))))
+        loss_one = tf.multiply(label, tf.log(tf.sigmoid(score)))
+        loss_zero = tf.multiply((1-label), tf.log((1 - tf.sigmoid(score))))
         loss += loss_one + loss_zero
-    #
-    # cos_eer, cos_thre = eval_kaldi_eer(cos_score, cos_label, cos=True, re_thre=True)
-    # p_cos_eer, p_cos_thre = eval_kaldi_eer(p_cos_score, cos_label, cos=True, re_thre=True)
 
     return -loss/batch_size
-
-    # return tf.cond(tf.equal(labels, 1), f1, f2)
-
 
 def eval_batch(batch_size, tuple_size, spk_representation, labels, l_weight, l_bias):
     '''
