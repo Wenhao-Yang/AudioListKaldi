1bfa726d71dfe6a4ee4250a4250f20344800a3e0
MacWilliam <874681044@qq.com>
2020-02-22 17:25:25 +0800

    prep vox1

--- local/make_voxceleb1.py
+++ local/make_voxceleb1.py
@@ -21,9 +21,9 @@ import csv
 import sys
 import pathlib
 
-train_dir = sys.argv[1]
-test_dir = sys.argv[2]
-
+data_root = sys.argv[1]
+train_dir = sys.argv[2]
+test_dir = sys.argv[3]
 for i in train_dir, test_dir:
     check_path = pathlib.Path(i)
     if not check_path.parent.exists():
@@ -55,20 +55,74 @@ def prep_u2s_ws(flistpath, id_idname, out_dir):
             uid2scp.append((uid, line))
             uid2idname.append((uid, id_idname[id]))
 
-    with open(os.path.join(out_dir, 'wav.scp'),'w') as f:
+    with open(os.path.join(out_dir, 'wav.scp'), 'w') as f:
         for e in uid2scp:
             f.writelines(str(e[0]) + ' ' + str(e[1]))
 
-    with open(os.path.join(out_dir, 'utt2spk'),'w') as f:
+    with open(os.path.join(out_dir, 'utt2spk'), 'w') as f:
         for e in uid2idname:
             f.writelines(str(e[0]) + ' ' + str(e[1]) + '\n')
 
-train_set_path = '/home/yangwenhao/projects/data/mydataset/voxceleb/voxceleb1_wav/vox1_dev_wav/'
-test_set_path = '/home/yangwenhao/projects/data/mydataset/voxceleb/voxceleb1_wav/vox1_test_wav/'
+def read_vox1_structure(directory, train_dir, test_dir):
+    """
+    :param directory:
+    :return: [{'subset': b'dev', 'speaker_id': b'id10133', 'filename': b'vox1_dev_wav/wav/id10133/wqLbqjw42L0/00001', 'uri': 0}...
+             ]
+    """
+    voxceleb = []
+    # /CDShare/voxceleb1/
+    data_root = pathlib.Path(directory)
+    data_root.cwd()
+    print('>>Data root is %s' % str(data_root))
 
-train_flist_path = os.path.join(train_set_path, 'wav.flist')
-test_flist_path = os.path.join(test_set_path, 'wav.flist')
+    # /CDShare/voxceleb1/vox1_test_wav/id10270/5r0dWxy17C8/00019.wav
+    all_wav_path = list(data_root.glob('*/*/*/*.wav'))
 
-id_idname_set = prep_id_idname('data/vox1_meta.csv')
-prep_u2s_ws(train_flist_path, id_idname_set, train_dir)
-prep_u2s_ws(test_flist_path, id_idname_set, test_dir)
+    dev_wav_path = []
+    test_wav_path = []
+    for wav in all_wav_path:
+        if wav.parents[2]=='vox1_test_wav':
+            test_wav_path.append(wav)
+        else:
+            dev_wav_path.append(wav)
+
+    wav_scp = open(os.path.join(train_dir, 'wav_scp'), 'w')
+    utt2spk = open(os.path.join(train_dir, 'utt2spk'), 'w')
+    for wav in dev_wav_path:
+        spkid = wav.parents[1].name
+        utt = wav.parents[0].name
+        uid = wav.name.rstrip('.wav')
+        uid = '-'.join(spkid, utt, uid)
+        wav_scp.write(uid + ' ' + str(wav) + '\n')
+        utt2spk.write(uid + ' ' + str(spkid) + '\n')
+
+    wav_scp.close()
+    utt2spk.close()
+    print('dev set preparing completed.')
+
+    wav_scp = open(os.path.join(test_dir, 'wav_scp'), 'w')
+    utt2spk = open(os.path.join(test_dir, 'utt2spk'), 'w')
+    for wav in test_wav_path:
+        spkid = wav.parents[1].name
+        utt = wav.parents[0].name
+        uid = wav.name.rstrip('.wav')
+        uid = '-'.join(spkid, utt, uid)
+        wav_scp.write(uid + ' ' + str(wav) + '\n')
+        utt2spk.write(uid + ' ' + str(spkid) + '\n')
+
+    wav_scp.close()
+    utt2spk.close()
+    print('test set preparing completed.')
+
+read_vox1_structure(data_root, train_dir, test_dir)
+#
+#
+# train_set_path = '/home/yangwenhao/projects/data/mydataset/voxceleb/voxceleb1_wav/vox1_dev_wav/'
+# test_set_path = '/home/yangwenhao/projects/data/mydataset/voxceleb/voxceleb1_wav/vox1_test_wav/'
+#
+# train_flist_path = os.path.join(train_set_path, 'wav.flist')
+# test_flist_path = os.path.join(test_set_path, 'wav.flist')
+#
+# id_idname_set = prep_id_idname('data/vox1_meta.csv')
+# prep_u2s_ws(train_flist_path, id_idname_set, train_dir)
+# prep_u2s_ws(test_flist_path, id_idname_set, test_dir)
---
+++ prepare_data/voxceleb.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+"""
+@Author: yangwenhao
+@Contact: 874681044@qq.com
+@Software: PyCharm
+@File: voxceleb.sh
+@Time: 2020/2/22 4:33 PM
+@Overview:
+"""
+
+export train_cmd="run.pl --mem 4G"
+export KALDI_ROOT=/Work18/2017/liumeng/Software/kaldi/
+export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sph2pipe_v2.5:$PWD:$PATH
+[ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
+. $KALDI_ROOT/tools/config/common_path.sh
+export LC_ALL=C
+
+set -e
+
+# The trials file is downloaded by local/make_voxceleb1.pl.
+vox1_trials=data/Vox1/trials
+vox1_root=/CDShare/voxceleb1
+vox2_root=/export/corpora/VoxCeleb2
+#nnet_dir=exp/xvector_nnet_1a
+#
+#res_dir=exp/resnt
+#tdnn_dir=exp/tdnn
+
+#musan_root=/export/corpora/JHU/musan
+vox1_out_dir=data/Vox1
+vox1_test_dir=data/Vox1/test
+vox1_train_dir=data/Vox1/dev
+
+mfccdir=${vox1_out_dir}/mfcc
+fbankdir=${vox1_out_dir}/fbank
+vaddir=${vox1_out_dir}/vad
+
+stage=0
+
+if [ $stage -le 0 ]; then
+  echo "===================================Data preparing=================================="
+  # This script creates data/voxceleb1_test and data/voxceleb1_train.
+  # Our evaluation set is the test portion of VoxCeleb1.
+  local/make_voxceleb1_trials.pl out_dir
+  local/make_voxceleb1.py ${vox1_root} ${vox1_train_dir} ${vox1_test_dir}
+
+  utils/utt2spk_to_spk2utt.pl $vox1_test_dir/utt2spk >$vox1_test_dir/spk2utt
+  utils/validate_data_dir.sh --no-text --no-feats $vox1_test_dir
+  utils/utt2spk_to_spk2utt.pl $vox1_train_dir/utt2spk >$vox1_train_dir/spk2utt
+  utils/validate_data_dir.sh --no-text --no-feats $vox1_train_dir
+
+fi
+
+if [ $stage -le 1 ]; then
+  # Make MFCCs and compute the energy-based VAD for each dataset
+  echo "==========================Making Fbank features and VAD============================"
+  for name in ${vox1_train_dir} ${vox1_test_dir}; do
+    steps/make_fbank.sh --write-utt2num-frames true --fbank_config conf/fbank.conf --nj 5 --cmd "$train_cmd" \
+        ${name} exp/make_fbank $fbankdir
+    utils/fix_data_dir.sh ${name}
+
+    # Todo: Is there any better VAD solutioin?
+    sid/compute_vad_decision.sh --nj 40 --cmd "$train_cmd" ${name} exp/make_vad $vaddir
+    utils/fix_data_dir.sh ${name}
+  done
+fi
