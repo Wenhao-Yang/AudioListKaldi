941513f6f3fa2a7d57cd8889e6e7a830ac7a739f
MacWilliam <874681044@qq.com>
2020-04-22 23:55:30 +0800

    color rgb

--- prepare_data/voxceleb.sh
+++ prepare_data/voxceleb.sh
@@ -156,7 +156,7 @@ if [ $stage -le 12 ]; then
   for name in dev test ; do
 #    local/nnet3/xvector/prepare_feats_for_cmvn.sh --nj 16 --cmd "$train_cmd" data/Vox1_pyfb/${name}_fb40 data/Vox1_pyfb/${name}_fb40_no_sil  data/Vox1_pyfb/${name}_fb40_no_sil/feats_no_sil
 #    utils/fix_data_dir.sh data/Vox1_pyfb/${name}_fb40_no_sil
-  local/nnet3/xvector/prepare_feats_for_cmvn.sh --nj 16 --cmd "$train_cmd" data/Vox1_spect/${name}_noc data/Vox1_spect/${name}_wcmvn  data/Vox1_spect/${name}__wcmvn/feats_no_sil
+  local/nnet3/xvector/prepare_feats_for_cmvn.sh --nj 16 --cmd "$train_cmd" data/Vox1_spect/${name}_noc data/Vox1_spect/${name}_wcmvn  data/Vox1_spect/${name}_wcmvn/feats_no_sil
     utils/fix_data_dir.sh data/Vox1_spect/${name}_wcmvn
   done
 fi
