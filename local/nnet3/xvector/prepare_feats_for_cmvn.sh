786f47be38a977352e7c3a9db7f5c061061a6d84
MacWilliam <874681044@qq.com>
2020-05-07 19:50:11 +0800

    4

--- local/nnet3/xvector/prepare_feats_for_cmvn.sh
+++ local/nnet3/xvector/prepare_feats_for_cmvn.sh
@@ -22,7 +22,7 @@ echo "$0 $@"  # Print the command line for logging
 if [ -f path.sh ]; then . ./path.sh; fi
 . ./utils/parse_options.sh || exit 1;
 
-if [ $# != 9 ]; then
+if [ $# != 3 ]; then
   echo $#
   echo "Usage: $0 <in-data-dir> <out-data-dir> <feat-dir>"
   echo "e.g.: $0 data/train data/train_no_sil exp/make_xvector_features"
