f6e14af977a5b1239f8406db7ecbcf29995c7212
MacWilliam <874681044@qq.com>
2020-06-23 21:37:08 +0800

    vox1

--- path.sh
+++ path.sh
@@ -1,4 +1,4 @@
-export KALDI_ROOT=/work20/yangwenhao/project/kaldi
+export KALDI_ROOT=/home/yangwenhao/local/project/kaldi
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sph2pipe_v2.5:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
