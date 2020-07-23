aabedc148495430f370ee202089c721ebebac564
MacWilliam <874681044@qq.com>
2020-07-23 20:27:21 +0800

    resample wav

--- utils/subset_domain_data.sh
+++ utils/subset_domain_data.sh
@@ -12,34 +12,34 @@ data_dir=$2
 out_dir=$3
 
 
-if [ -f $data/utt2dom ]; then
-  rm $data/utt2dom
+if [ -f $data_dir/utt2dom ]; then
+  rm $data_dir/utt2dom
 fi
 
 mkdir -p $out_dir || exit 1
 #[ ! -e $out_dir ] && rm $data/utt.tmp
 
-touch "$data/utt2dom"
+touch "$data_dir/utt2dom"
 
-[ -f $data/utt2dom ] && rm $data/utt.tmp
-touch "$data/utt.tmp"
+[ -f $data_dir/utt2dom ] && rm $data_dir/utt.tmp
+touch "$data_dir/utt.tmp"
 
-if [ -f $data/utt2dom ]; then
+if [ -f $data_dir/utt2dom ]; then
 
   echo "$0: getting domains of utterances from utt2dom files"
-  domains=`cat $data/utt2dom | sort -k 2 | awk '{print $2}' | uniqe`
+  domains=`cat $data_dir/utt2dom | sort -k 2 | awk '{print $2}' | uniqe`
   for d in domains ; do
-    num_utt=`cat $data/utt2dom | grep $d | wc -l `
+    num_utt=`cat $data_dir/utt2dom | grep $d | wc -l `
     c=`echo $(echo "$num_utt*$num_utt_per"|bc) | awk '{print int($0)}'`
 
-    cat $data/utt2dom | grep $d | shuf | head -$c | awk '{print $1}' >> $data/utt.tmp
+    cat $data_dir/utt2dom | grep $d | shuf | head -$c | awk '{print $1}' >> $data_dir/utt.tmp
 
   done
 
-  utils/subset_data_dir.sh --utt-list $data/utt.tmp $data_dir $out_dir
-  rm $data/utt.tmp
+  utils/subset_data_dir.sh --utt-list $data_dir/utt.tmp $data_dir $out_dir
+  rm $data_dir/utt.tmp
 
 fi
 
-echo "$0: completed partition using $data/utt2dom"
+echo "$0: completed partition using $data_dir/utt2dom"
 exit 0;
