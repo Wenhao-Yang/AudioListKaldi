36d6f4b14ce5952b0ae638d3599b15fa990c77f4
MacWilliam <874681044@qq.com>
2020-09-17 20:17:13 +0800

    vox1

--- prepare_data/radio_noise.sh
+++ prepare_data/radio_noise.sh
@@ -4,9 +4,11 @@ stage=0
 
 if [ $stage -le 0 ]; then
   python local/make_radio.py --dataset-dir /home/storage/yangwenhao/dataset/wav_test \
-    --output-dir data/radio/noise
+    --output-dir data/radio
 fi
 
 if [ $stage -le 1 ]; then
-  local/resample_data_dir.sh /home/yangwenhao/storage/dataset/wav_test 8000 data/radio/noise data/radio/noise_8k
+  for name in noise, sample ;do
+    local/resample_data_dir.sh /home/yangwenhao/storage/dataset/wav_test 8000 data/radio/${name} data/radio/${name}_8k
+  done
 fi
