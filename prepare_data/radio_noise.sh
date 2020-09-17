dc19548634fb50c1835699a6e62c15eb8157dbc8
MacWilliam <874681044@qq.com>
2020-09-17 20:44:05 +0800

    vox1

--- prepare_data/radio_noise.sh
+++ prepare_data/radio_noise.sh
@@ -8,7 +8,7 @@ if [ $stage -le 0 ]; then
 fi
 
 if [ $stage -le 1 ]; then
-  for name in noise, sample ;do
+  for name in noise sample ;do
     local/resample_data_dir.sh /home/yangwenhao/storage/dataset/wav_test 8000 data/radio/${name} data/radio/${name}_8k
   done
 fi
