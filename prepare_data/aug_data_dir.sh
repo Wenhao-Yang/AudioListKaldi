8bbfb96298ddd45110aeb6203c1d8785bff0a8bb
MacWilliam <874681044@qq.com>
2020-09-17 23:59:11 +0800

    vox1

--- prepare_data/aug_data_dir.sh
+++ prepare_data/aug_data_dir.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-stage=5
+stage=6
 
 if [ $stage -le 0 ]; then
   dataset=aishell2
@@ -25,10 +25,19 @@ if [ $stage -le 5 ]; then
                             --suffix 8k
   done
 fi
+if [ $stage -le 6 ]; then
+  dataset=vox1
+  for name in dev test; do
+    [ ! -f data/${dataset}/${name}_8k/reco2dur ] && utils/data/get_reco2dur.sh data/${dataset}/${name}_8k
+    steps/data/augment_data_dir.py --utt-suffix "radio" --bg-snrs " -15:-20:-25:-30" --num-bg-noises "1" --bg-noise-dir "data/radio/noise_8k" data/vox1/${name}_8k data/vox1/${name}_8k_radio
 
+    prepare_data/aug2wav.py --dataset-dir /home/storage/yangwenhao/dataset/voxceleb_8k --outset-dir /home/storage/yangwenhao/dataset/voxceleb_8k_radio_%s --data-dir data/vox1 --set-name ${name}_8k_radio
+  done
+
+fi
 
 stage=10
-if [ $stage -le 6 ]; then
+if [ $stage -le 7 ]; then
   dataset=vox1
   for name in dev test; do
     [ ! -f data/${dataset}/${name}_8k/reco2dur ] && utils/data/get_reco2dur.sh data/${dataset}/${name}_8k
