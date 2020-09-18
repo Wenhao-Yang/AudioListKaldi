d6f38b51643b24e0bcfab43636f530e7be3337d2
MacWilliam <874681044@qq.com>
2020-09-18 20:08:50 +0800

    vox1

--- prepare_data/aug_data_dir.sh
+++ prepare_data/aug_data_dir.sh
@@ -26,16 +26,16 @@ if [ $stage -le 5 ]; then
   done
 fi
 
-if [ $stage -le 6 ]; then
-  dataset=vox1
-  for name in test ; do
-    [ ! -f data/${dataset}/${name}_8k_wav/reco2dur ] && utils/data/get_reco2dur.sh data/vox1/${name}_8k_wav
-    steps/data/augment_data_dir.py --utt-suffix "radio" --bg-snrs " -15:-20:-25:-30" --num-bg-noises "1" --bg-noise-dir "data/radio/noise_8k" data/vox1/${name}_8k_wav data/vox1/${name}_8k_radio
-
-    prepare_data/aug2wav.py --dataset-dir /home/storage/yangwenhao/dataset/voxceleb_8k --outset-dir /home/storage/yangwenhao/dataset/voxceleb_8k_radio_%s --data-dir data/vox1 --set-name ${name}_8k_radio
-  done
-
-fi
+#if [ $stage -le 6 ]; then
+#  dataset=vox1
+#  for name in test ; do
+#    [ ! -f data/${dataset}/${name}_8k_wav/reco2dur ] && utils/data/get_reco2dur.sh data/vox1/${name}_8k_wav
+#    steps/data/augment_data_dir.py --utt-suffix "radio" --bg-snrs " -15:-20:-25:-30" --num-bg-noises "1" --bg-noise-dir "data/radio/noise_8k" data/vox1/${name}_8k_wav data/vox1/${name}_8k_radio
+#
+#    prepare_data/aug2wav.py --dataset-dir /home/storage/yangwenhao/dataset/voxceleb_8k --outset-dir /home/storage/yangwenhao/dataset/voxceleb_8k_radio_%s --data-dir data/vox1 --set-name ${name}_8k_radio
+#  done
+#
+#fi
 
 stage=10
 if [ $stage -le 7 ]; then
@@ -49,3 +49,8 @@ if [ $stage -le 7 ]; then
 
 fi
 
+if [ $stage -le 8 ]; then
+  for name in dev test ; do
+    utils/combine_data.sh data/army/spect/vox1_${name}_clear_radio data/vox1/spect/${name}_8k_radio_v2 data/vox1/spect/${name}_8k
+  done
+fi
