3ad96ff01b8ebf18cb9742ca66633b36b4fd0cdd
william-asus <874681044@qq.com>
2020-11-10 23:36:06 +0800

    nj12

--- local/resample_data_dir.sh
+++ local/resample_data_dir.sh
@@ -23,6 +23,7 @@ echo "New wavs will be writen to ${out_data}"
 
 nj=0
 #[ ! -f $out_dir/wav.scp ] && touch $out_dir/wav.scp
+all_job=12
 
 cat $data_dir/wav.scp | \
     while read line; do
@@ -32,14 +33,14 @@ cat $data_dir/wav.scp | \
           # /home/storage/yangwenhao/dataset/voxceleb2/dev/aac/id00012/21Uxsk56VDQ/00010.wav
           orig_path=${l[-1]} #/home/cca01/work2019/yangwenhao/mydataset/wav_test/noise/CHN01/D01-U000000.wav
           new_path=${orig_path/"$org_data"/"$out_data"}
-          echo $orig_path $new_path
+          # echo $orig_path $new_path
 
           [ ! -d ${new_path%/*} ] && mkdir -p ${new_path%/*}
           sox -V1 ${orig_path} -r $sample_rate ${new_path} &
           echo -e "${l[-2]} ${new_path}\n" >> $out_dir/wav.scp
 
           nj=`expr $nj + 1`
-          if [ $(( $nj % 18 ))} = 0 ]; then
+          if [ $(( $nj % $all_job ))} = 0 ]; then
             wait
           fi
         fi
