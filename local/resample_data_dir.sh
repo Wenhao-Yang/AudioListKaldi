97db2882a3364b308fdc9ff381e4032e04b6073a
Mac-Yang <874681044@qq.com>
2021-03-03 21:36:22 +0800

    [prepare] ressample -s

--- local/resample_data_dir.sh
+++ local/resample_data_dir.sh
@@ -32,17 +32,21 @@ cat $data_dir/wav.scp | \
           # echo ${#l[@]}
           # /home/storage/yangwenhao/dataset/voxceleb2/dev/aac/id00012/21Uxsk56VDQ/00010.wav
           orig_path=${l[-1]} #/home/cca01/work2019/yangwenhao/mydataset/wav_test/noise/CHN01/D01-U000000.wav
-          new_path=${orig_path/"$org_data"/"$out_data"}
-          # echo $orig_path $new_path
 
-          [ ! -d ${new_path%/*} ] && mkdir -p ${new_path%/*}
-          sox -V1 ${orig_path} -r $sample_rate ${new_path} &
-          echo -e "${l[-2]} ${new_path}\n" >> $out_dir/wav.scp
+          if [ -s ${orig_path} ]; then
+            new_path=${orig_path/"$org_data"/"$out_data"}
+            # echo $orig_path $new_path
 
-          nj=`expr $nj + 1`
-          if [ $(( $nj % $all_job ))} = 0 ]; then
-            wait
+            [ ! -d ${new_path%/*} ] && mkdir -p ${new_path%/*}
+            sox -V1 ${orig_path} -r $sample_rate ${new_path} &
+            echo -e "${l[-2]} ${new_path}\n" >> $out_dir/wav.scp
+
+            nj=`expr $nj + 1`
+            if [ $(( $nj % $all_job ))} = 0 ]; then
+              wait
+            fi
           fi
+
         fi
 
     done
