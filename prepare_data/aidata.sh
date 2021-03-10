aa401ed88c5f213c08e08f076e462cea0a26bdc9
Mac-Yang <874681044@qq.com>
2021-03-10 19:43:51 +0800

    [data] aidata

---
+++ prepare_data/aidata.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# """
+# @Author: yangwenhao
+# @Contact: 874681044@qq.com
+# @Software: PyCharm
+# @File: sitw.sh
+# @Time: 2020/3/13 16:13 PM
+# @Overview:
+# """
+
+
+stage=0
+if [ $stage -le 0 ]; then
+  ls *.tar.gz | xargs -n1 tar xzvf
+fi
+
+
+if [ $stage -le 5 ]; then
+  python local/make_aidata.py --dataset-dir /home/storage/yangwenhao/dataset/aidatatang_200zh/corpus \
+    --output-dir data/aidata
+fi
