4ec58b4aef7cc9f0a9c665ec8cc1d81a48ef0eed
Mac-Yang <874681044@qq.com>
2021-03-12 17:19:05 +0800

    [local] make trials rm unce

--- prepare_data/aidata.sh
+++ prepare_data/aidata.sh
@@ -17,6 +17,6 @@ fi
 
 
 if [ $stage -le 5 ]; then
-  python local/make_aidata.py --dataset-dir /home/storage/yangwenhao/dataset/aidatatang_200zh/corpus \
+  python local/make_aidata.py --dataset-dir /home/work2020/yangwenhao/dataset/aidatatang_200zh/aidatatang_200zh/corpus \
     --output-dir data/aidata
 fi
