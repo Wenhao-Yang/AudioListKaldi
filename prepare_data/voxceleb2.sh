ab8cae55b4ba16a46ecad405579f2a26bf574f97
Mac-Yang <874681044@qq.com>
2021-06-11 14:24:06 +0800

    [data] make fb80 for vox1

--- prepare_data/voxceleb2.sh
+++ prepare_data/voxceleb2.sh
@@ -86,9 +86,9 @@ if [ $stage -le 3 ]; then
   # This script applies CMVN and removes nonspeech frames.  Note that this is somewhat
   # wasteful, as it roughly doubles the amount of training data on disk.  After
   # creating training examples, this can be removed.
-  for data_dir in  data/vox1/klfb/test_fb40 ; do
-    steps/make_fbank.sh --nj 6 --cmd "$train_cmd" \
-     --fbank-config conf/fbank_40.conf \
+  for data_dir in data/vox1/klfb/dev_fb80 data/vox1/klfb/test_fb80 ; do
+    steps/make_fbank.sh --nj 12 --cmd "$train_cmd" \
+     --fbank-config conf/fbank_80.conf \
      --write-utt2num-frames true \
      --write-utt2dur true \
      $data_dir
