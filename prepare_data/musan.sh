634f1efb06d7d49ecac33e37bb532eb18585e1b2
Mac-Yang <874681044@qq.com>
2021-12-02 17:05:23 +0800

    [data] musan fb40

--- prepare_data/musan.sh
+++ prepare_data/musan.sh
@@ -10,7 +10,7 @@
 . ./path.sh
 set -e
 
-stage=0
+stage=1
 dataset=musan
 
 if [ $stage -le 0 ]; then
@@ -23,4 +23,23 @@ if [ $stage -le 0 ]; then
     utils/fix_data_dir.sh data/${dataset}/klsp/${name}
   done
 
+fi
+
+if [ $stage -le 1 ]; then
+  name=fb40
+  dataset=musan
+  feat=klfb
+
+  for name in music_fb40 noise_fb40 ; do # dev_aug_fb40
+    steps/make_fbank.sh --write-utt2num-frames true --fbank-config conf/fbank_40.conf \
+      --nj 14 --cmd "$train_cmd" \
+      data/${dataset}/klfb/${name} data/${dataset}/klfb/${name}/log data/${dataset}/klfb/fbank/${name}
+    utils/fix_data_dir.sh data/${dataset}/klfb/${name}
+  done
+
+#  for s in music noise; do
+#    python local/split_trials_dir.py --data-dir data/${dataset}/${feat}/dev_${name} \
+#      --out-dir data/${dataset}/${feat}/dev_${name}/trials_dir \
+#      --trials trials_2w
+#  done
 fi
