24d736259097fbf5f171a4a8cba305702734dfb5
Mac-Yang <874681044@qq.com>
2021-12-31 19:43:31 +0800

    [data] v2 fb24 fb64 fb80

--- prepare_data/voxceleb2.sh
+++ prepare_data/voxceleb2.sh
@@ -117,9 +117,10 @@ if [ $stage -le 5 ]; then
   # Make Spectrogram for aug set
   dataset=vox2
   echo "===================              Fbank               ========================"
-  for name in dev ; do
-    steps/make_fbank.sh --write-utt2num-frames true --fbank-config conf/fbank_40.conf \
-      --nj 12 data/${dataset}/klfb/${name}_fb40 data/${dataset}/klfb/${name}_fb40/log data/${dataset}/klfb/fbank/${name}_fb40
+  for dim in 24 64 80; do
+    name=dev_fb${dim}
+    steps/make_fbank.sh --write-utt2num-frames true --fbank-config conf/fbank_${dim}.conf \
+      --nj 12 data/${dataset}/klfb/${name} data/${dataset}/klfb/${name}/log data/${dataset}/klfb/fbank/${name}
   done
     # Todo: Is there any better VAD solutioin?
 #  sid/compute_vad_decision.sh --nj 12 --cmd "$train_cmd" ${vox1_org_dir}/test exp/make_vad ${vox1_org_dir}/vad
