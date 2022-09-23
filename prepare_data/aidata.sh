d482559d56626203bde72da4355295aa55c59564
william-asus <874681044@qq.com>
2022-09-23 23:56:54 +0800

    [data] aidata

--- prepare_data/aidata.sh
+++ prepare_data/aidata.sh
@@ -25,12 +25,24 @@ if [ $stage -le 5 ]; then
   ./local/resample_data_dir.sh /home/work2020/yangwenhao/dataset/aidatatang_200zh/aidatatang_200zh/corpus/dev 8000 data/aidata/dev data/aidata/dev_8k
 fi
 
+
 if [ $stage -le 10 ]; then
 
-  for dim in 40; do # dev_aug_fb40
-    name=dev_fb${dim}
-    steps/make_fbank.sh --write-utt2num-frames true --fbank-config conf/fbank_${dim}.conf \
-      --nj 14 --cmd "$train_cmd" \
-      data/aidata/klfb/${name} data/aidata/klfb/${name}/log data/aidata/klfb/fbank/${name}
-    utils/fix_data_dir.sh data/aidata/klfb/${name}
+  for dim in 40 ; do # dev_aug_fb40
+    for sets in train; do
+      name=${sets}_fb${dim}
+      if [ ! -d data/aidata/klfb/${name} ]; then
+        utils/copy_data_dir.sh data/aidata/${sets} data/aidata/klfb/${name}
+      fi
+      steps/make_fbank.sh --write-utt2num-frames true --fbank-config conf/fbank_${dim}.conf \
+        --nj 14 --cmd "$train_cmd" \
+        data/aidata/klfb/${name} data/aidata/klfb/${name}/log data/aidata/klfb/fbank/${name}
+      utils/fix_data_dir.sh data/aidata/klfb/${name}
   done
+  done
+
+  exit
+
+# steps/make_fbank.sh --write-utt2num-frames true --fbank-config conf/fbank_40.conf \
+#       --nj 12 data/vox1/klfb/test_fb40 data/vox1/klfb/test_fb40/log data/vox1/klfb/fbank/test_fb40
+fi
