815aae5230b6819df381d0b555284018838345fd
william-asus <874681044@qq.com>
2022-09-24 00:00:13 +0800

    [data] aidata train

--- prepare_data/aidata.sh
+++ prepare_data/aidata.sh
@@ -9,6 +9,15 @@
 # @Overview:
 # """
 
+export train_cmd="run.pl --mem 16G"
+
+export KALDI_ROOT=/home/yangwenhao/local/project/kaldi
+export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sph2pipe_v2.5:$PWD:$PATH
+[ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
+. $KALDI_ROOT/tools/config/common_path.sh
+export LC_ALL=C
+
+set -e
 
 stage=10
 if [ $stage -le 0 ]; then
