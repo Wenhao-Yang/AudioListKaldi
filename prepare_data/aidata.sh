4ad81e50dcaa2f913352e33f81adfa7bea632557
Wenhao-Yang A315 <874681044@qq.com>
2023-03-08 09:54:28 +0800

    [data] aidata compress

--- prepare_data/aidata.sh
+++ prepare_data/aidata.sh
@@ -41,12 +41,12 @@ if [ $stage -le 10 ]; then
     for sets in train test; do
       name=${sets}_fb${dim}
       if [ ! -d data/aidata/klfb/${name} ]; then
-        utils/copy_data_dir.sh data/aidata/${sets} data/aidata/klfb/${name}
+        utils/copy_data_dir.sh data/aidata/${sets} data/aidata/klfb/${name}_ncm
       fi
-      steps/make_fbank.sh --write-utt2num-frames true --fbank-config conf/fbank_${dim}.conf \
+      steps/make_fbank.sh --compress false --write-utt2num-frames true --fbank-config conf/fbank_${dim}.conf \
         --nj 14 --cmd "$train_cmd" \
-        data/aidata/klfb/${name} data/aidata/klfb/${name}/log data/aidata/klfb/fbank/${name}
-      utils/fix_data_dir.sh data/aidata/klfb/${name}
+        data/aidata/klfb/${name}_ncm data/aidata/klfb/${name}_ncm/log data/aidata/klfb/fbank/${name}_ncm
+      utils/fix_data_dir.sh data/aidata/klfb/${name}_ncm
   done
   done
 
